817b9ee4c438f17526e19bb4790209a7920e27cb
sluxz3r <rurul161011@gmail.com>
2019-07-08 11:24:37 +0700

    new

---
+++ src/component/header1.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Header1 extends Component{
+    render() {
+        return (
+            <div>
+                <p>Header Kecil</p>
+            </div>
+        );
+    }
+}
+
+export default Header1;
---
+++ src/component/header2.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Header2 extends Component{
+    render() {
+        return (
+            <div>
+                <p>Header Besar</p>
+            </div>
+        );
+    }
+}
+
+export default Header2;
