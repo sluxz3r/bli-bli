9508ec6462b53a0bee981e0fefa2c6590714fafb
sluxz3r <rurul161011@gmail.com>
2019-07-08 14:32:13 +0700

    neww

--- src/component/header1.js
+++ src/component/header1.js
@@ -9,26 +9,47 @@ class Header1 extends Component {
                     <ul>
                         <li>
                             <img src="http://www.gelarukm.com/assets/category/handphone.png" className="hp"></img>
-                            <a className="mobile" href="#">Mobile App Download</a>
+                            <a className="mobile" href="#">Mobile App Download &#709;</a>
                             <ul>
                                 <li>
-                                    <img src="https://www.materialui.co/materialIcons/action/android_grey_192x192.png"/>
+                                    <img src="https://www.materialui.co/materialIcons/action/android_grey_192x192.png"/><br/>
                                     <a href="#">Android</a>
                                 </li>
                                 <li>
-                                    <img src="https://www.mbsit.co.uk/wp-content/uploads/2018/07/apple_icon-icons.com_62745.png?w=640"/>
+                                    <img src="https://www.mbsit.co.uk/wp-content/uploads/2018/07/apple_icon-icons.com_62745.png?w=640"/><br/>
                                     <a href="#">iOS</a>
                                 </li>
                                 <li>
-                                    <img src="https://cdn2.iconfinder.com/data/icons/ourea-icons/256/OS_Windows_8_256x256-32.png" />
+                                    <img src="https://cdn2.iconfinder.com/data/icons/ourea-icons/256/OS_Windows_8_256x256-32.png" /><br/>
                                     <a href="#">Windows</a>
                                 </li>
+                                <i className="dwn">Download Bangsad!!!!!</i>
                             </ul>
+                           
                         </li>
                     </ul>
                 </div>
                 <div className="right-header">
-                    <a>Bantuan 24x7</a>
+                <ul>
+                        <li>
+                            <a className="bantuan" href="#">Bantuan 24x7  &#709;</a>
+                            <ul>
+                                <li>
+                                    <a href="#">Pembayaran</a>
+                                </li>
+                                <li>
+                                    <a href="#">Pengiriman</a>
+                                </li>
+                                <li>
+                                    <a href="#">Pengembalian</a>
+                                </li>
+                                <li>
+                                    <a href="#">Panduan Lainnya</a>
+                                </li>
+                            </ul>                        
+                           
+                        </li>
+                    </ul>
                     <a>Jual di Blibli.com</a>
                     <a>Blibli Rewards</a>
                     <a>Cek Status Pesanan</a>
