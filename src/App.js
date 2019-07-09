76a55a87812b3a2412ec5e1ccec7a7e0c6ce1025
auliarampit <auliarahmat25599@gmail.com>
2019-07-09 18:08:03 +0700

    neww

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import React, {Component} from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
 import Header1 from './component/header1';
 import Header2 from './component/header2';
-import Homepage from './component/homepage'
-import FlashSale from './component/flashsale';
 import Footer from './component/footer';
-import Inspirasi from './component/inspirasi';
-import Category from './component/category';
 import PageHome from './pages/pageHome';
 import PageCategory from './pages/pagesCategory';
 import './App.css';
