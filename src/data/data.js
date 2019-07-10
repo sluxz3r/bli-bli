790e75e1133e920103db1993b0c9cd641ed4b8c9
bimtut <masbimtut@gmail.com>
2019-07-10 17:31:05 +0700

    edit data lagi

--- src/data/data.js
+++ src/data/data.js
@@ -4,12 +4,13 @@ const Data= [
         name: "Mi 9",
         image: "https://icdn2.digitaltrends.com/image/xioami-mi-9-review-7-720x720.jpg",
         category: "Handphone",         
-        price: "RP5.150.000",
-        description: [
+        price: 150000,
+        features: [
             'uhbuhbugbyhgby',
             'iuniunuynuh',
             'inhnhnuu uu'
-        ]
+        ],
+        description: "ykbiubasjkffbabf hadbhsb fbahdfab"
     },
     {
         id: "2",
@@ -17,9 +18,12 @@ const Data= [
         image: "https://i5.walmartimages.com/asr/3032cbb2-3565-43e5-af35-a0aa76ff8bb0_1.06114a4d2d6870f733056fdb5e405da0.jpeg",
         category: "Peralatan Elektronik",
         price: 200000,
-        description: {
-            
-        }
+        features: [
+            'uhbuhbugbyhgby',
+            'iuniunuynuh',
+            'inhnhnuu uu'
+        ],
+        description: "ykbiubasjkffbabf hadbhsb fbahdfab"
     },
     {
         id: "3",
@@ -27,9 +31,12 @@ const Data= [
         image: "https://i.gadgets360cdn.com/products/large/1555920099_635_realme3_pro_db.jpg",
         category: "Handphone",         
         price: 2.000000,
-        description: {
-            
-        }
+        features: [
+            'uhbuhbugbyhgby',
+            'iuniunuynuh',
+            'inhnhnuu uu'
+        ],
+        description: "ykbiubasjkffbabf hadbhsb fbahdfab"
     },
     {
         id: "4",
@@ -37,9 +44,12 @@ const Data= [
         image: "https://cdn.sindonews.net/webp/620/content/2019/05/24/123/1407034/fujifilm-gfx-100-Peralatan Elektronik-medium-harga-rp140-juta-dengan-sensor-102-mp-loz.webp",
         category: "Peralatan Elektronik",
         price: 500000,
-        description: {
-            
-        }
+        features: [
+            'uhbuhbugbyhgby',
+            'iuniunuynuh',
+            'inhnhnuu uu'
+        ],
+        description: "ykbiubasjkffbabf hadbhsb fbahdfab"
     },
     {
         id: "5",
@@ -47,9 +57,12 @@ const Data= [
         image: "https://d2giyh01gjb6fi.cloudfront.net/default/0002/83/thumb_182868_default_big.jpeg",
         category: "Handphone",         
         price: 2277000,
-        description: {
-            
-        }
+        features: [
+            'uhbuhbugbyhgby',
+            'iuniunuynuh',
+            'inhnhnuu uu'
+        ],
+        description: "ykbiubasjkffbabf hadbhsb fbahdfab"
     },
     {
         id: "7",
@@ -57,9 +70,12 @@ const Data= [
         image: "https://cdn.idntimes.com/content-images/community/2018/04/7976332159-137e970c8c-b-844f7d54e4e704e1739da52fba329f3d_600x400.jpg",
         category: "Peralatan Elektronik",
         price: 350000,
-        description: {
-            
-        }
+        features: [
+            'uhbuhbugbyhgby',
+            'iuniunuynuh',
+            'inhnhnuu uu'
+        ],
+        description: "ykbiubasjkffbabf hadbhsb fbahdfab"
     },
     {
         id: "8",
@@ -67,9 +83,12 @@ const Data= [
         image: "https://aerish.com/wp-content/uploads/2018/12/Barangimpordotnet-Matras_yoga_original_kettler-0004.jpg",
         category: "Fashion Wanita",
         price: 180000,
-        description: {
-            
-        }
+        features: [
+            'uhbuhbugbyhgby',
+            'iuniunuynuh',
+            'inhnhnuu uu'
+        ],
+        description: "ykbiubasjkffbabf hadbhsb fbahdfab"
     },
     {
         id: "9",
@@ -77,9 +96,12 @@ const Data= [
         image: "http://1.bp.blogspot.com/-b4JxSvi7Ydw/UH4-rXv53cI/AAAAAAAACHs/obOq388qMXE/s1600/IMG01398-20121017-0941.jpg",
         category: "Handphone",         
         price: 450000,
-        description: {
-            
-        }
+        features: [
+            'uhbuhbugbyhgby',
+            'iuniunuynuh',
+            'inhnhnuu uu'
+        ],
+        description: "ykbiubasjkffbabf hadbhsb fbahdfab"
     },
     {
         id: "10",
@@ -87,9 +109,12 @@ const Data= [
         image: "https://ecs7.tokopedia.net/img/cache/700/product-1/2019/6/22/3990375/3990375_1ec3e734-97c5-4b3b-a376-741e43d6f52e_1000_1000",
         category: "Fashion Pria",
         price: 120000,
-        description: {
-            
-        }
+        features: [
+            'uhbuhbugbyhgby',
+            'iuniunuynuh',
+            'inhnhnuu uu'
+        ],
+        description: "ykbiubasjkffbabf hadbhsb fbahdfab"
     },
     {
         id: "11",
@@ -97,9 +122,12 @@ const Data= [
         image: "https://ecs7.tokopedia.net/img/cache/700/product-1/2018/6/24/3092596/3092596_91de7f4b-e8ea-4ed9-9508-67a908f573a9.jpeg",
         category: "Fashion Wanita",
         price: 80000,
-        description: {
-            
-        }
+        features: [
+            'uhbuhbugbyhgby',
+            'iuniunuynuh',
+            'inhnhnuu uu'
+        ],
+        description: "ykbiubasjkffbabf hadbhsb fbahdfab"
     },
     {
         id: "12",
@@ -107,9 +135,12 @@ const Data= [
         image: "https://ecs7.tokopedia.net/img/cache/700/product-1/2015/5/10/331515/331515_30cdcb10-f6fd-11e4-9dda-ab9b49bc7260.jpg",
         category: "Fashion Wanita",
         price: 50000,
-        description: {
-            
-        }
+        features: [
+            'uhbuhbugbyhgby',
+            'iuniunuynuh',
+            'inhnhnuu uu'
+        ],
+        description: "ykbiubasjkffbabf hadbhsb fbahdfab"
     },
     {
         id: "13",
@@ -117,9 +148,12 @@ const Data= [
         image: "https://ecs7.tokopedia.net/img/cache/700/product-1/2017/4/26/153950655/153950655_09222366-ffba-4062-93ac-55cece82ae3b_528_528.jpg",
         category: "Fashion Pria",
         price: 35000,
-        description: {
-            
-        }
+        features: [
+            'uhbuhbugbyhgby',
+            'iuniunuynuh',
+            'inhnhnuu uu'
+        ],
+        description: "ykbiubasjkffbabf hadbhsb fbahdfab"
     },
     {
         id: "14",
@@ -127,9 +161,12 @@ const Data= [
         image: "https://cf.shopee.co.id/file/7fc5b524885b465d7cf1fd57312163c9",
         category: "Fashion Wanita",
         price: 3000,
-        description: {
-            
-        }
+        features: [
+            'uhbuhbugbyhgby',
+            'iuniunuynuh',
+            'inhnhnuu uu'
+        ],
+        description: "ykbiubasjkffbabf hadbhsb fbahdfab"
     },
     {
         id: "15",
@@ -137,9 +174,12 @@ const Data= [
         image: "https://cdn.runrepeat.com/i/nike/24925/nike-men-s-lunar-apparent-black-white-cool-grey-8-m-us-men-s-black-white-cool-grey-5d65-600.jpg",
         category: "Fashion Pria",
         price: 99000,
-        description: {
-            
-        }
+        features: [
+            'uhbuhbugbyhgby',
+            'iuniunuynuh',
+            'inhnhnuu uu'
+        ],
+        description: "ykbiubasjkffbabf hadbhsb fbahdfab"
     },
     {
         id: "16",
@@ -147,9 +187,12 @@ const Data= [
         image: "https://img.solopos.com/posts/2018/12/08/957603/d0bed0b1d0bbd0bed0b6d0bad0b0_d0b8d0b3d180d18b_crash_team_racing-f9f2.jpg?w=600",
         category: "Tiket",
         price: 780000,
-        description: {
-            
-        }
+        features: [
+            'uhbuhbugbyhgby',
+            'iuniunuynuh',
+            'inhnhnuu uu'
+        ],
+        description: "ykbiubasjkffbabf hadbhsb fbahdfab"
     },
     {
         id: "17",
@@ -157,9 +200,12 @@ const Data= [
         image: "https://i.pinimg.com/originals/18/61/61/18616113367054cb2fba488c7d6e6e2c.jpg",
         category: "Tiket",
         price: 20000,
-        description: {
-            
-        }
+        features: [
+            'uhbuhbugbyhgby',
+            'iuniunuynuh',
+            'inhnhnuu uu'
+        ],
+        description: "ykbiubasjkffbabf hadbhsb fbahdfab"
     },
     {
         id: "18",
@@ -167,9 +213,12 @@ const Data= [
         image: "https://cf.shopee.co.id/file/85e0894619ede501a2a60a954e73e3a8",
         category: "Fashion Pria",
         price: 250,
-        description: {
-            
-        }
+        features: [
+            'uhbuhbugbyhgby',
+            'iuniunuynuh',
+            'inhnhnuu uu'
+        ],
+        description: "ykbiubasjkffbabf hadbhsb fbahdfab"
     },
     {
         id: "19",
@@ -177,9 +226,12 @@ const Data= [
         image: "https://www.gbuksystems.com/content/images/thumbs/0002198_dell-latitude-e7250.jpeg",
         category: "Komputer",
         price: 5170000,
-        description: {
-            
-        }
+        features: [
+            'uhbuhbugbyhgby',
+            'iuniunuynuh',
+            'inhnhnuu uu'
+        ],
+        description: "ykbiubasjkffbabf hadbhsb fbahdfab"
     },
     {
         id: "20",
@@ -187,9 +239,12 @@ const Data= [
         image: "https://s2.bukalapak.com/img/7485317414/w-1000/1865622_07601a12_98af_47ba_b7c1_2c122e48f0e4.jpg",
         category: "Tiket",
         price: 15000,
-        description: {
-            
-        }
+        features: [
+            'uhbuhbugbyhgby',
+            'iuniunuynuh',
+            'inhnhnuu uu'
+        ],
+        description: "ykbiubasjkffbabf hadbhsb fbahdfab"
     },
     {
         id: "21",
@@ -197,9 +252,12 @@ const Data= [
         image: "https://images.latintimes.com/sites/latintimes.com/files/styles/large/public/2014/08/13/hungry-shark-evolution.jpg",
         category: "Tiket",
         price: 7000,
-        description: {
-            
-        }
+        features: [
+            'uhbuhbugbyhgby',
+            'iuniunuynuh',
+            'inhnhnuu uu'
+        ],
+        description: "ykbiubasjkffbabf hadbhsb fbahdfab"
     },
     {
         id: "22",
@@ -207,9 +265,12 @@ const Data= [
         image: "https://www.quietpc.com/images/products/coffeelake-pentium-large.jpg",
         category: "Komputer",
         price: 85000,
-        description: {
-            
-        }
+        features: [
+            'uhbuhbugbyhgby',
+            'iuniunuynuh',
+            'inhnhnuu uu'
+        ],
+        description: "ykbiubasjkffbabf hadbhsb fbahdfab"
     },
     {
         id: "24",
@@ -217,9 +278,12 @@ const Data= [
         image: "https://images-na.ssl-images-amazon.com/images/I/8107%2BoAFSmL._SL1500_.jpg",
         category: "Komputer",
         price: 50000,
-        description: {
-            
-        }
+        features: [
+            'uhbuhbugbyhgby',
+            'iuniunuynuh',
+            'inhnhnuu uu'
+        ],
+        description: "ykbiubasjkffbabf hadbhsb fbahdfab"
     },
     {
         id: "25",
@@ -227,9 +291,12 @@ const Data= [
         image: "https://uksaah.com/wp-content/uploads/2018/10/011-min.png",
         category: "Komputer",
         price: 85000,
-        description: {
-            
-        }
+        features: [
+            'uhbuhbugbyhgby',
+            'iuniunuynuh',
+            'inhnhnuu uu'
+        ],
+        description: "ykbiubasjkffbabf hadbhsb fbahdfab"
     }
 ]
 
