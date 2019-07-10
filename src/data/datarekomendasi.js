abfb4b119251bed64a137dbf1d7000ad09477078
bimtut <masbimtut@gmail.com>
2019-07-10 12:32:27 +0700

    produk terlaris

---
+++ src/data/datarekomendasi.js
@@ -0,0 +1,64 @@
+const dataRekomendasi = [
+    {
+        id: "17",
+        name: "Pepsi Man",
+        image: "https://i.pinimg.com/originals/18/61/61/18616113367054cb2fba488c7d6e6e2c.jpg",
+        category: "Tiket",
+        price: "Rp20.000",
+        description: {
+            
+        }
+    },
+    {
+        id: "18",
+        name: "Papan Karambol",
+        image: "https://cf.shopee.co.id/file/85e0894619ede501a2a60a954e73e3a8",
+        category: "Fashion Pria",
+        price: "Rp250",
+        description: {
+            
+        }
+    },
+    {
+        id: "19",
+        name: "Dell Latitude E7250",
+        image: "https://www.gbuksystems.com/content/images/thumbs/0002198_dell-latitude-e7250.jpeg",
+        category: "Komputer",
+        price: "Rp5.170.000",
+        description: {
+            
+        }
+    },
+    {
+        id: "20",
+        name: "Obat Tetes Mata",
+        image: "https://s2.bukalapak.com/img/7485317414/w-1000/1865622_07601a12_98af_47ba_b7c1_2c122e48f0e4.jpg",
+        category: "Tiket",
+        price: "Rp15.000",
+        description: {
+            
+        }
+    },
+    {
+        id: "21",
+        name: "Megalodon",
+        image: "https://images.latintimes.com/sites/latintimes.com/files/styles/large/public/2014/08/13/hungry-shark-evolution.jpg",
+        category: "Tiket",
+        price: "Rp17.000",
+        description: {
+            
+        }
+    },
+    {
+        id: "22",
+        name: "Intel Pentium",
+        image: "https://www.quietpc.com/images/products/coffeelake-pentium-large.jpg",
+        category: "Komputer",
+        price: "Rp1.850.000",
+        description: {
+            
+        }
+    }
+]
+
+export default dataRekomendasi;
